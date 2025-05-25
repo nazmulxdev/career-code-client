cf7656ee4ad1fb1d98e90576fd0e3f32c2f00ffb
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-05-25 22:50:00 +0600

    initialized carrer code job portal client site

---
+++ src/Router/Router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Root";
+import Home from "../Pages/Home";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+export default Router;
